404486ec889900ba95351c39b36d4882fc6f8643
Denis Onichshenko <alienden@gmail.com>
2017-11-13 13:41:54 -0500

    Added base template and demo InfluxDB command

--- webstats.py
+++ webstats.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 from flask import jsonify
 from influxdb import InfluxDBClient
 
@@ -6,10 +7,11 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('settings.cfg', silent=True)
 
 @app.route("/")
-def hello():
+def graph():
     client = InfluxDBClient(host=app.config['INFLUX_HOST'], port=app.config['INFLUX_PORT'])
     dbs = client.get_list_database()
-    return jsonify(dbs)
+    return render_template('index.html',
+                           results=dbs)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
