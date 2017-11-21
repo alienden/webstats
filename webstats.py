6f887f49e18d59b6ea3ddf3ec64aea063da2c7ad
Denis Onichshenko <alienden@gmail.com>
2017-11-21 18:39:06 -0500

    First revision of plotly graph introduced

--- webstats.py
+++ webstats.py
@@ -2,16 +2,48 @@ from flask import Flask
 from flask import render_template
 from flask import jsonify
 from influxdb import InfluxDBClient
+import json
+import plotly
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('settings.cfg', silent=True)
 
 @app.route("/")
 def graph():
+    timestamps = []
+    powervalues = []
+
     client = InfluxDBClient(host=app.config['INFLUX_HOST'], port=app.config['INFLUX_PORT'], database=app.config['INFLUX_DATABASE'])
-    query = client.query('select time, power_consumption from W where time > now() - 24h')
+    #get all the power measurments from InfluxDB for the last 24hr
+    result = client.query('select time, power_consumption from W where time > now() - 24h')
+
+    datapoints = list(result.get_points())
+    header_list = list(datapoints[0].keys())
+    #get list of values for x,y axis
+    for point in datapoints:
+        timestamps.append(point['time'])
+        powervalues.append(point['power_consumption'])
+
+    #setup graph
+    graphs = [
+        dict(
+            data=[
+                dict(
+                    x=timestamps,
+                    y=powervalues,
+                    type='scatter'
+                ),
+            ],
+            layout=dict(
+                title='Power Consumption from PC outlet'
+            )
+        )
+    ]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+
+
     return render_template('index.html',
-                           results=query.get_points())
+                           graphJSON=graphJSON)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
